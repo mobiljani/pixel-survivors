25c235872a0b5108e70c023b8d4c5fcaac103cb5
mobiljani <egybemail@gmail.com>
2025-05-07 12:14:19 +0100

    Walk animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
+    [SerializeField] private float speed;
+
+    public Vector3 playerMoveDirection;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,6 +21,23 @@ public class PlayerController : MonoBehaviour
         var inputX = Input.GetAxisRaw("Horizontal");
         var inputY = Input.GetAxisRaw("Vertical");
 
-        rb.linearVelocity = new Vector2(inputX, inputY);
+        playerMoveDirection = new Vector3(inputX, inputY).normalized;
+
+        animator.SetFloat("moveX", inputX);
+        animator.SetFloat("moveY", inputY);
+
+        if (playerMoveDirection != Vector3.zero)
+        {
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(playerMoveDirection.x * speed, playerMoveDirection.y * speed);
     }
 }
