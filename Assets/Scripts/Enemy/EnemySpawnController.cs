b161d58d1c67c32bc27db84a37ce2a96e063793b
mobiljani <egybemail@gmail.com>
2025-05-09 08:21:58 +0100

    Spawn multiple enemies

--- Assets/Scripts/Enemy/EnemySpawnController.cs
+++ Assets/Scripts/Enemy/EnemySpawnController.cs
@@ -1,25 +1,51 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawnerCopntroller : MonoBehaviour
+
+
+
+public class EnemySpawnController : MonoBehaviour
 {
-    public GameObject enemyPrefab;
-    public float spawnInterval;
-    private float spawnTimer;
-    // Update is called once per frame
+    [Serializable]
+    public class Wave
+    {
+        public GameObject enemyPrefab;
+        public int noOfEnemyToSpawn = 2;
+        public int currentCount;
+        public float spawnTimer;
+        public float spawnInterval = 2f;
+
+    }
+
+    public List<Wave> waves;
+    private int waveIndex = 0;
+
     void Update()
     {
-        spawnTimer += Time.deltaTime;
-        if (spawnTimer >= spawnInterval)
+        var w = waves[waveIndex];
+        w.spawnTimer += Time.deltaTime;
+        if (w.spawnTimer >= w.spawnInterval)
         {
             SpawnEnemy();
-            spawnTimer = 0f;
+            w.currentCount++;
+            w.spawnTimer = 0;
         }
 
+        if (w.currentCount == w.noOfEnemyToSpawn)
+        {
+            waveIndex++;
+            w.currentCount = 0;
+        }
+
+        if (waves.Count == waveIndex)
+        {
+            waveIndex = 0;
+        }
     }
 
     private void SpawnEnemy()
     {
-        Instantiate(enemyPrefab, transform.position, transform.rotation);
+        Instantiate(waves[waveIndex].enemyPrefab, transform.position, transform.rotation);
     }
 }
